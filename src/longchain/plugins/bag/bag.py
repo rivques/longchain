b0674f80444a4dca7e88eeaff0e86dcf7c2bbb29
rivques <38469076+rivques@users.noreply.github.com>
2024-08-25 10:42:35 -0400

    it helps if you actually finish writing the function

--- src/longchain/plugins/bag/bag.py
+++ src/longchain/plugins/bag/bag.py
@@ -25,21 +25,22 @@ class BagManager:
         if not keyresult.valid:
             raise ValueError("Invalid Bag key. Check that your app id and key are correct.")
     
-    def get_inventory(self, identity_id: str) -> RCFContainer[bag_pb2.Instance]:
+    def get_inventory(self, identity_id: str) -> bag_pb2.GetInventoryResponse:
         if self.stub is None:
             raise ValueError("BagManager not configured. Call bm_instance.configure() first.")
         result: bag_pb2.GetInventoryResponse = self.stub.GetInventory(bag_pb2.GetInventoryRequest(appId=self.app_id, key=self.key, identityId=identity_id))
-        return result.inventory
+        return result
     
-    def create_instance(self, identity_id: str, item_id: str, quantity: int,  metadata: str = "{}", public: bool = True, show: bool = True, note: str = "") -> bag_pb2.Instance:
+    def create_instance(self, identity_id: str, item_id: str, quantity: int,  metadata: str = "{}", public: bool = True, show: bool = True, note: str = "") -> bag_pb2.CreateInstanceResponse:
         if self.stub is None:
             raise ValueError("BagManager not configured. Call bm_instance.configure() first.")
         result: bag_pb2.CreateInstanceResponse = self.stub.CreateInstance(bag_pb2.CreateInstanceRequest(appId=self.app_id, key=self.key, identityId=identity_id, itemId=item_id, quantity=quantity, metadata=metadata, public=public, show=show, note=note))
-        return result.instance
+        return result
     
-    def make_offer(self, target_identity_id: str, offer_to_give: RCFContainer[bag_pb2.OfferItem], offer_to_receive: RCFContainer[bag_pb2.OfferItem]):
+    def make_offer(self, target_identity_id: str, offer_to_give: RCFContainer[bag_pb2.OfferItem], offer_to_receive: RCFContainer[bag_pb2.OfferItem]) -> bag_pb2.MakeOfferResponse:
         if self.stub is None:
             raise ValueError("BagManager not configured. Call bm_instance.configure() first.")
-        result = self.stub.MakeOffer(bag_pb2.MakeOfferRequest(appId=self.app_id, key=self.key, sourceIdentityId=self.owner_id, ))
+        result = self.stub.MakeOffer(bag_pb2.MakeOfferRequest(appId=self.app_id, key=self.key, sourceIdentityId=self.owner_id, targetIdentityId=target_identity_id, offerToGive=offer_to_give, offerToReceive=offer_to_receive))
+        return result
 bag_instance = BagManager()
 
